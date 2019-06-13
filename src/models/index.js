e94462d1357db38844f6c6a79eadf41645d79b8e
muguira-james <muguira.james@gmail.com>
2019-06-13 11:11:56 -0400

    heroku changes still

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,21 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
+let sequelize;
+if (process.env.DATABASE_URL) {
+  console.log("prod db url-->", process.env.DATABASE_URL)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-  },
-);
-
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 const models = {
   User: sequelize.import('./user'),
   Message: sequelize.import('./message'),
@@ -20,68 +27,5 @@ Object.keys(models).forEach(key => {
   }
 });
 
-export { sequelize };
-
-export default models;
-
-
-/*
-# Write your query or mutation here
-# {
-#   user(id: "2") {
-#     username
-#   }
-# 	me {
-#     username
-#   }
-#   users {
-#     username
-#     id
-#   }
-# }
-
-# query {
-# 	message(id: "1") {
-#     id
-#     text
-#   }
-# }
-
-# mutation {
-#   createMessage(text: "this is my message") {
-#     id
-#     text
-#   }
-# }
-
-mutation {
-  deleteMessage(id: "be44dcd8-0318-4d44-afd0-f485cd1d7791")
-}
-*/
-
-// let users = {
-//   1: {
-//     id: '1',
-//     username: 'James Muguira',
-//   },
-//   2: {
-//     id: '2',
-//     username: 'Julie Ray',
-//   },
-// };
-
-// const me = users[1]
-
-
-// let myMessages = {
-//   1: {
-//     id: '1',
-//     text: 'Hello World',
-//   },
-//   2: {
-//     id: '2',
-//     text: 'Bye World',
-//   },
-// };
-
-// module.exports = { users, me, myMessages } 
+export { sequelize }
+ export default models ;
